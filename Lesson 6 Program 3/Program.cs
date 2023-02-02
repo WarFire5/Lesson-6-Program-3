b3eb4758367052cb9e7eba3d1e62083e340ab0b5
Татьяна <@WIN-E362SIULNK8>
2023-02-03 02:05:00 +0300

    Добавьте файлы проекта.

---
+++ Lesson 6 Program 3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_6_Program_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дано линейное уравнение стандартного вида: A*X+B=C.");
+
+            string A, B, C, X;
+
+            Console.WriteLine("Задайте число 'A':");
+            A = Console.ReadLine();
+
+            Console.WriteLine("Задайте число 'B':");
+            B = Console.ReadLine();
+
+            Console.WriteLine("Задайте число 'C':");
+            C = Console.ReadLine();
+
+            int a = Convert.ToInt32(A);
+
+            int b = Convert.ToInt32(B);
+
+            int c = Convert.ToInt32(C);
+
+            int x = (c - b) / a;
+
+            X = "Ответ: X=" + x;
+
+            Console.WriteLine(X);
+
+            Console.ReadLine();
+        }
+    }
+}
